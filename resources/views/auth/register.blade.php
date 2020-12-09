1104f679666a6c58da530e6166edc6c1805dc31d
Spilock <34496260+luqmanulhakim96@users.noreply.github.com>
2020-12-09 11:23:53 +0800

    added login, register, forget password

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,76 +1,50 @@
-@extends('layouts.app')
+@extends('layouts.app-login-register')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+          <form class="splash-container">
+              <div class="card">
+                  <div class="card-header">
+                      <h3 class="mb-1">Registrations Form</h3>
+                      <p>Please enter your user information.</p>
+                  </div>
+                  <div class="card-body">
+                      <div class="form-group">
+                          <input class="form-control form-control-lg" type="text" name="nick" required="" placeholder="Username" autocomplete="off">
+                      </div>
+                      <div class="form-group">
+                          <input class="form-control form-control-lg" type="email" name="email" required="" placeholder="E-mail" autocomplete="off">
+                      </div>
+                      <div class="form-group">
+                          <input class="form-control form-control-lg" id="pass1" type="password" required="" placeholder="Password">
+                      </div>
+                      <div class="form-group">
+                          <input class="form-control form-control-lg" required="" placeholder="Confirm">
+                      </div>
+                      <div class="form-group pt-2">
+                          <button class="btn btn-block btn-primary" type="submit">Register My Account</button>
+                      </div>
+                      <div class="form-group">
+                          <label class="custom-control custom-checkbox">
+                              <input class="custom-control-input" type="checkbox"><span class="custom-control-label">By creating an account, you agree the <a href="#">terms and conditions</a></span>
+                          </label>
+                      </div>
+                      <!-- <div class="form-group row pt-0">
+                          <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb-2">
+                              <button class="btn btn-block btn-social btn-facebook " type="button">Facebook</button>
+                          </div>
+                          <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                              <button class="btn  btn-block btn-social btn-twitter" type="button">Twitter</button>
+                          </div>
+                      </div> -->
+                  </div>
+                  <div class="card-footer bg-white">
+                      <p>Already member? <a href="{{route('login')}}" class="text-secondary">Login Here.</a></p>
+                  </div>
+              </div>
+          </form>
         </div>
     </div>
 </div>
---
+++ resources/views/layouts/app-login-register.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <script src="{{ asset('concept/vendor/jquery/jquery-3.3.1.min.js ') }}"></script>
+    <script src="{{ asset('concept/vendor/bootstrap/js/bootstrap.bundle.js ') }}"></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('concept/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link href="{{ asset('concept/vendor/fonts/circular-std/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('concept/libs/css/style.css') }}">
+    <link rel="stylesheet" href=" {{ asset('concept/vendor/fonts/fontawesome/css/fontawesome-all.css') }}">
+
+</head>
+<body>
+    <div id="app">
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
