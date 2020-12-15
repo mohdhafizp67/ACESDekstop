8c894f1d577dcf6ff2d0b141d486f8fa3f725567
amsyarDev <61406633+amsyarDev@users.noreply.github.com>
2020-12-15 17:25:35 +0800

    push
    - update profile admin
    - user list
    - enable and disable user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ use Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AdminController extends Controller
 {
@@ -21,14 +22,43 @@ class AdminController extends Controller
     return view('admin.profiles.edit-profile', compact('user'));
   }
 
+  public function updatingProfile(){
+    $this->validator(request()->all())->validate();
+
+    $user_id = Auth::user()->id;
+    $user = User::findOrFail($user_id);
+
+    $user->name = request()->name;
+    $user->ic_number = request()->ic_number;
+    $user->email = request()->email;
+    $user->phone = request()->phone;
+    $user->address = request()->address;
+    $user->postcode = request()->postcode;
+    $user->state = request()->state;
+    $user->save();
+
+    return redirect()->route('admin.profiles.edit-profile')->with("success","Profile has been updated");
+  }
+
+  protected function validator(array $data)
+  {
+      return Validator::make($data, [
+        'name' =>['required', 'string'],
+        'ic_number' =>['required', 'string'],
+        'email' =>['required', 'email'],
+        'phone' =>['required', 'string'],
+        'address' => ['required', 'string'],
+        'postcode' =>['required'],
+        'state' =>['required', 'string'],
+      ]);
+  }
+
   public function viewChangePassword()
   {
       return view('admin.profiles.change-password');
   }
 
   public function updatingPassword(Request $request){
-    // dd($request->all());
-
     $user_id = Auth::user()->id;
     $user = User::findOrFail($user_id);
 
@@ -38,12 +68,10 @@ class AdminController extends Controller
 
     if(strcmp($request->get('password'),$request->get('new_password'))== 0){
       return redirect()->back()->with("error","Current Password cannot be the same as New Password");
-      //dd('password lama sama dgn pass baru');
     }
 
     if(strcmp($request->get('new_password'),$request->get('password_confirmation'))== 1){
       return redirect()->back()->with("error","New Password not same as Confirmation Password");
-      //dd('password baru tak sama');
     }
 
     $validatedData = $request->validate([
@@ -58,12 +86,30 @@ class AdminController extends Controller
       $user->save();
 
       return redirect()->route('admin.profiles.change-password')->with("success","Password has been changed");
+  }
 
-      // if($request->language == "english"){
-      //   return redirect()->route('user.mainMenu_eng')->with("success","Password has been changed");
-      // }else {
-      //   return redirect()->route('user.mainMenu')->with("success","Kata laluan telah ditukar");
-      // }
+  public function viewUserList()
+  {
+      $user_list = User::get();
+      return view('admin.others.user-list', compact('user_list'));
+  }
+
+  public function disableStatus(Request $request)
+  {
+    $user = User::findOrFail($request->id_disable);
+    $user->is_active = 0;
+    $user->save();
+
+    return redirect()->route('admin.others.user-list')->with("success","User has been disabled");
+  }
+
+  public function activateStatus(Request $request)
+  {
+    $user = User::findOrFail($request->id_activate);
+    $user->is_active = 1;
+    $user->save();
+
+    return redirect()->route('admin.others.user-list')->with("success","User has been activated");
   }
 
 }
