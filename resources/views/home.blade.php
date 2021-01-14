0345941da24e460846f161fb258df6ee337b9e3b
luqmanulhakim96 <hakim9608@gmail.com>
2021-01-14 13:34:31 +0800

    add announcement in student's profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,7 +26,7 @@
 				ACES <font color="#CF00D8">I 4.0</font>
 			</h1>
 			<p style="text-align: justify;padding-bottom: 5%; color: #fff;">
-				Maklumat Terperinci>
+				Maklumat Terperinci
 			</p>
       <p style="color: #fff;"><img alt="Bootstrap Image Preview" src="{{asset('concept/images/videopelajaran/FaceIcon.png')}}" style="width: 10%; margin-left: -72%; padding-bottom: 5%;">{{Auth::user()->name}}</p>
 			<div class="row" style="padding-bottom: 2%;">
@@ -55,7 +55,14 @@
 			</h3>
 			<div class="row">
 				<div class="col-md-12" style="background-image: linear-gradient(to right top, #031f48, #232d74, #54349c, #9030bd, #d300d3); border: solid 10px; border-color: #1E1E21">
-					<img alt="Bootstrap Image Preview" src="{{asset('concept/images/StudentDashboard/ps4.jpg')}}" style="width: 50%; height: auto; background-position: 50% 50%;background-repeat: no-repeat;background-size: cover; padding-bottom: 10%; margin-top: 15%;">
+          <br>
+          <p style="color: #fff;text-align: center; font-size: 20px;">
+              @foreach($announcement as $data)
+              {{$data->user_message}}
+              <br>
+              @endforeach
+    			</p>
+          <img alt="Bootstrap Image Preview" src="{{asset('concept/images/StudentDashboard/ps4.jpg')}}" style="width: 50%; height: auto; background-position: 50% 50%;background-repeat: no-repeat;background-size: cover; padding-bottom: 10%; margin-top: 15%;">
 				</div>
 			</div>
 		</div>
