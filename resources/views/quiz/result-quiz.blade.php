7a191d7052d4172997a35ce2221ba9fd4fdc6180
mohdhafiz <61405917+mohdhafizp67@users.noreply.github.com>
2021-01-27 17:30:45 +0800

    quiz

--- resources/views/quiz/result-quiz.blade.php
+++ resources/views/quiz/result-quiz.blade.php
@@ -3,109 +3,134 @@
 @section('content')
 
 
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>QUIZ RESULT</title>
+  </head>
+  <body>
 
-<div class="dashboard-ecommerce">
-    <div class="container-fluid dashboard-content ">
-        <div class="ecommerce-widget">
-          <div class="card" style="padding: 10px; background-color: #9966ff !important;">
+<div class="container-fluid" style="background-image: url({{asset('concept/images/galaxy/background_blue.png')}}); background-position: 40% 40%;background-repeat: no-repeat;background-size: cover;">
+  <h1 style="padding-top: 3%; text-align: center; color: #fff; font-weight: bold;">QUIZ SCORE</h1>
 
-            <div style="padding: 5px;"></div>
-              <h2 class="card-header" style="text-align: center;"><i class="fa fa-question-circle mr-2" aria-hidden="true"></i>Keputusan</h2>
-              <div class="card-body p-0">
-                <div style="padding: 10px;"></div>
-                <div class="row">
-                  <div class="col-md-2">
+<div class="row" style="padding-top: 5%; padding-bottom: 8%;">
+  <div class="col-md-12">
 
-                  </div>
-                  <div class="col-md-3">
-                    <label>Pelajaran :</label>
-                  </div>
-                  <div class="col-md-3">
-                    <label>{{$student_quiz->quiz->lesson->lesson_type}} | {{$student_quiz->quiz->lesson->lesson_subject}}</label>
-                  </div>
-                  <div class="col-md-2">
+    <div class="row">
+      <div class="col-md-2">
 
-                  </div>
+      </div>
+      <div class="col-md-8">
+        <div class="card-body" style="padding: 10%;background-color: transparent !important; border: transparent; background-image: url({{asset('concept/images/galaxy/cards2.png')}}); background-repeat: no-repeat;background-size: 100% 100%">
+
+
+            <div class="card-body p-0">
+              <div style="padding: 0px;"></div>
+              <div class="row">
+                <div class="col-md-1">
+
                 </div>
-
-                <div class="row">
-                  <div class="col-md-2">
-
-                  </div>
-                  <div class="col-md-3">
-                    <label>Markah :</label>
-                  </div>
-                  <div class="col-md-3">
-                    <label> {{$student_quiz->result}} / {{$student_quiz->quiz->number_of_question}}</label>
-                  </div>
-                  <div class="col-md-2">
-
-                  </div>
+                <div class="col-md-4" style="font-size: 200%; color: #fff;">
+                  <label>Pelajaran :</label>
                 </div>
-
-                <div class="row">
-                  <div class="col-md-2">
-
-                  </div>
-                  <div class="col-md-3">
-                    <label>Peratusan :</label>
-                  </div>
-                  <div class="col-md-3">
-                    <label> {{$student_quiz->percentage}} %</label>
-                  </div>
-                  <div class="col-md-2">
-
-                  </div>
+                <div class="col-md-6" style="font-size: 200%; color: #fff;">
+                  <label>{{$student_quiz->quiz->lesson->lesson_type}} | {{$student_quiz->quiz->lesson->lesson_subject}}</label>
                 </div>
+                <div class="col-md-1">
 
-                <div class="row">
-                  <div class="col-md-2">
-
-                  </div>
-                  <div class="col-md-3">
-                    <label>Soalan yang telah dijawab :</label>
-                  </div>
-                  <div class="col-md-3">
-                    <label> {{$student_quiz->answered_question}} </label>
-                  </div>
-                  <div class="col-md-2">
-
-                  </div>
-                </div>
-
-                <div class="row">
-                  <div class="col-md-2">
-
-                  </div>
-                  <div class="col-md-3">
-                    <label>Status :</label>
-                  </div>
-                  <div class="col-md-3">
-                    <label> {{$student_quiz->result_status}}  </label>
-                  </div>
-                  <div class="col-md-2">
-
-                  </div>
-                </div>
-
-                <div style="padding: 10px;"></div>
-
-                <div class="row">
-                  <div class="col-md-2">
-
-                  </div>
-                  <div class="col-md">
-                    <a href="{{route('quiz.choose-quiz')}}" class="btn btn-block btn-secondary">Kembali Ke Senarai Quiz</a>
-                  </div>
-                  <div class="col-md-2">
-
-                  </div>
                 </div>
               </div>
-          </div>
+
+              <div class="row">
+                <div class="col-md-1">
+
+                </div>
+                <div class="col-md-4" style="font-size: 200%; color: #fff;">
+                  <label>Markah :</label>
+                </div>
+                <div class="col-md-6" style="font-size: 200%; color: #fff;">
+                  <label> {{$student_quiz->result}} / {{$student_quiz->quiz->number_of_question}}</label>
+                </div>
+                <div class="col-md-1">
+
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-1">
+
+                </div>
+                <div class="col-md-4" style="font-size: 200%; color: #fff;">
+                  <label>Peratusan :</label>
+                </div>
+                <div class="col-md-6" style="font-size: 200%; color: #fff;">
+                  <label> {{$student_quiz->percentage}} %</label>
+                </div>
+                <div class="col-md-1">
+
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-1">
+
+                </div>
+                <div class="col-md-4" style="font-size: 200%; color: #fff;">
+                  <label>Soalan yang telah dijawab :</label>
+                </div>
+                <div class="col-md-6" style="font-size: 200%; color: #fff;">
+                  <label> {{$student_quiz->answered_question}} </label>
+                </div>
+                <div class="col-md-1">
+
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-1">
+
+                </div>
+                <div class="col-md-4" style="font-size: 200%; color: #fff;">
+                  <label>Status :</label>
+                </div>
+                <div class="col-md-6" style="font-size: 200%; color: #fff;">
+                  <label> {{$student_quiz->result_status}}  </label>
+                </div>
+                <div class="col-md-1">
+
+                </div>
+              </div>
+
+              <div style="padding: 10px;"></div>
+
+              <div class="row">
+                <div class="col-md-2">
+
+                </div>
+                <div class="col-md">
+                  <a href="{{route('quiz.choose-quiz')}}" class="btn btn-block" style="background-image: url({{asset('concept/images/galaxy/button_submit.png')}}); background-position: 50% 50%;background-repeat: no-repeat;background-size: cover;">Kembali Ke Senarai Quiz</a>
+                </div>
+                <div class="col-md-2">
+
+                </div>
+              </div>
+            </div>
         </div>
+      </div>
+      <div class="col-md-2">
+
+      </div>
     </div>
+
+  </div>
 </div>
+
+</div>
+
+  </body>
+</html>
+
 <script type="text/javascript">
 function pass_quiz_id(id){
  $(".modal-footer #quiz_id").val( id );
