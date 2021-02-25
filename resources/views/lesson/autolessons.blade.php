f3d298d2868c655b599284dbb285d0f28fbf938f
mohdhafizp67 <mohdhafizp67@gmail.com>
2021-02-25 14:56:53 +0800

    push baru

--- resources/views/lesson/autolessons.blade.php
+++ resources/views/lesson/autolessons.blade.php
@@ -218,7 +218,7 @@
                         </ul>
                         <div class="tab-content" id="myTabContent">
                             <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                              <iframe class="embed-responsive-item" width="95%" height="450px" src="https://www.youtube.com/embed/qYnVqJ6ko8I?autoplay=0&controls=0&disablekb=1&rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                              <iframe class="embed-responsive-item" width="95%" height="450px" src="https://www.youtube.com/embed/D6RHOsMsoW0?autoplay=0&controls=0&disablekb=1&rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             </div>
 
                             <div class="tab-pane fade" id="link" role="tabpanel" aria-labelledby="profile-tab" style="width:95%; height:450px;">
