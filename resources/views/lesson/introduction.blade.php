831f402351d5b30273a5de72516fd468a21dc29c
mohdhafizp67 <mohdhafizp67@gmail.com>
2021-03-05 15:15:14 +0800

    push baru

--- resources/views/lesson/introduction.blade.php
+++ resources/views/lesson/introduction.blade.php
@@ -528,17 +528,17 @@
 
           window.onload = function () {
             //display timer
-            var fiveMinutes = 5 * 1,
+            var fiveMinutes = 180 * 1,
                 display = document.querySelector('#time');
             startTimer(fiveMinutes, display);
 
             //timer for auto submit form/quiz
-            var setTimer = (1 * 5) * 1000;
+            var setTimer = (1 * 180) * 1000;
             // var setTimer = 5000;
             window.setTimeout(function() {
               $.ajax({
                 type:"get",
-                 url:"/public/lesson/ajax/update-lesson/1",
+                 url:"/ACES-Desktop/lesson/ajax/update-lesson/1",
 
                 success: function(respond){
                 },
