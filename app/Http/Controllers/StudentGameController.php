bef295238a62269c7190ab1bd5c6917a43606294
mohdhafizp67 <mohdhafizp67@gmail.com>
2021-03-10 16:49:59 +0800

    push student game update

--- app/Http/Controllers/StudentGameController.php
+++ app/Http/Controllers/StudentGameController.php
@@ -44,6 +44,47 @@ class StudentGameController extends Controller
                 $student_game = Student_Game::find($student_game_search[0]->id);
             }
 
+            $student_id = Auth::user()->student->id;
+
+            $check_student_game = Student_Game::where('student_id',  $student_id)->where('game_id',$request->$game_id)->count();
+
+            if($check_student_game == 0){
+
+              $student_game = Student_Game::Create([
+
+                'student_point' => $student_point,
+                'game_id' => $request->game_id,
+                'student_id' => $student_id,
+                ]);
+
+
+                $leaderboard = Leaderboard::where('student_id', $student_id)->first();
+
+                $leaderboard->scores = $leaderboard->scores + $student_point;
+                $leaderboard->save();
+            }else {
+              $student_game = Student_Game::where('student_id', $student_id)->where('game_id', $request->game_id)->first();
+
+                  if($student_point > $game_student->student_point){
+
+                    $leaderboard = Leaderboard::where('student_id', $student_id)->first();
+                    $leaderboard->scores = ($leaderboard->scores - $student_game->student_point) + $student_point;
+                    $leaderboard->save();
+
+                    $student_game->student_point;
+                    $student_game->game_id = $request->game_id;
+                    $student_game->student_id = $student_id;
+                    $student_game->save();
+                  }
+
+                      else {
+                        $student_game->student_point;
+                        $student_game->game_id = $request->game_id;
+                        $student_game->student_id = $student_id;
+                        $student_game->save();
+                      }
+            }
+
 
             $student_game->student_point = $request->student_point;
             $student_game->game_id = $request->game_id;
@@ -59,4 +100,5 @@ class StudentGameController extends Controller
 
         return response()->json($response);
     }
+
 }
