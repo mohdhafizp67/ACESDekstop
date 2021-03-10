94ee7f5e32825582ca90d3eba5ad310fdca8ef3b
mohdhafizp67 <mohdhafizp67@gmail.com>
2021-03-10 12:02:53 +0800

    push student game controller

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -25,60 +25,59 @@ class GameController extends Controller
 
   }
 
-  // public function update_game($game_id){
-  //   $student_id = Auth::user()->student->id;
-  //
-  //   $check_game_student = Student_Game::where('student_id',  $student_id)->where('game_id', $game_id)->count();
-  //   if($check_game_student == 0){
-  //     event($game_student_id = $this->create_game_student($game_id));
-  //   }
-  //   exit;
-  // }
+  public function update_game($game_id){
+    $student_id = Auth::user()->student->id;
+
+    $check_game_student = Student_Game::where('student_id',  $student_id)->where('game_id', $game_id)->count();
+    if($check_game_student == 0){
+      event($game_student_id = $this->create_game_student($game_id));
+    }
+    exit;
+  }
 
 // update game point
-    public function update_game($game_id){
-      $student_id = Auth::user()->student->id;
-
-      $check_game_student = Student_Game::where('student_id',  $student_id)->where('game_id',$request->$game_id)->count();
-      if($check_game_student == 0){
-
-        $game_student = Student_Game::Create([
-
-          'student_point' => $student_point,
-          'game_id' => $request->game_id,
-          'student_id' => $student_id,
-          ]);
-
-          //update leaderboards
-          // dd($student_quiz);
-          $leaderboard = Leaderboard::where('student_id', $student_id)->first();
-          // dd($leaderboard);
-          $leaderboard->scores = $leaderboard->scores + $student_point;
-          $leaderboard->save();
-      }else {
-        $game_student = Student_Game::where('student_id', $student_id)->where('game_id', $request->game_id)->first();
-
-            if($student_point > $game_student->student_point){
-              //update leaderboard
-              $leaderboard = Leaderboard::where('student_id', $student_id)->first();
-              $leaderboard->scores = ($leaderboard->scores - $game_student->student_point) + $student_point;
-              $leaderboard->save();
-
-              $game_student->student_point;
-              $game_student->game_id = $request->game_id;
-              $game_student->student_id = $student_id;
-              $game_student->save();
-            }
-
-                else {
-                  $game_student->student_point;
-                  $game_student->game_id = $request->game_id;
-                  $game_student->student_id = $student_id;
-                  $game_student->save();
-                }
-      }
-      exit;
-    }
+    // public function update_game($game_id){
+    //   $student_id = Auth::user()->student->id;
+    //
+    //   $check_game_student = Student_Game::where('student_id',  $student_id)->where('game_id',$request->$game_id)->count();
+    //   if($check_game_student == 0){
+    //
+    //     $game_student = Student_Game::Create([
+    //
+    //       'student_point' => $student_point,
+    //       'game_id' => $request->game_id,
+    //       'student_id' => $student_id,
+    //       ]);
+    //
+    //
+    //       $leaderboard = Leaderboard::where('student_id', $student_id)->first();
+    //
+    //       $leaderboard->scores = $leaderboard->scores + $student_point;
+    //       $leaderboard->save();
+    //   }else {
+    //     $game_student = Student_Game::where('student_id', $student_id)->where('game_id', $request->game_id)->first();
+    //
+    //         if($student_point > $game_student->student_point){
+    //
+    //           $leaderboard = Leaderboard::where('student_id', $student_id)->first();
+    //           $leaderboard->scores = ($leaderboard->scores - $game_student->student_point) + $student_point;
+    //           $leaderboard->save();
+    //
+    //           $game_student->student_point;
+    //           $game_student->game_id = $request->game_id;
+    //           $game_student->student_id = $student_id;
+    //           $game_student->save();
+    //         }
+    //
+    //             else {
+    //               $game_student->student_point;
+    //               $game_student->game_id = $request->game_id;
+    //               $game_student->student_id = $student_id;
+    //               $game_student->save();
+    //             }
+    //   }
+    //   exit;
+    // }
 
   public function create_game_student($game_id)
   {
