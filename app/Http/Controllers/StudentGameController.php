c98980caae57de3a264f474a00ca31de4a5b2045
luqmanulhakim96 <hakim9608@gmail.com>
2021-03-29 10:01:18 +0800

    add api for mobile, change env example

--- app/Http/Controllers/StudentGameController.php
+++ app/Http/Controllers/StudentGameController.php
@@ -58,4 +58,52 @@ class StudentGameController extends Controller
 
         return response()->json($response);
     }
+
+    public function createMobile(Request $request)
+    {
+
+        $user_ic =  $request->encoded_ic;
+
+        $response = new stdClass();
+
+        try {
+            $student = DB::table('students')
+            ->join('users', 'users.id', '=', 'students.user_id')
+            ->select('students.id')
+            ->where("users.ic_number", "=", $user_ic)
+                ->get();
+
+            if (count($student) > 1) {
+                $response->success = false;
+                $response->message = "";
+                return response()->json($response);
+            }
+
+            $student_game_search = DB::table('students_games')
+            ->where("student_id", "=", $student[0]->id)
+                ->where("game_id", "=", $request->game_id)
+                ->get();
+
+            if (count($student_game_search) === 0) {
+                $student_game = new Student_Game();
+            }
+
+            if (count($student_game_search) === 1) {
+                $student_game = Student_Game::find($student_game_search[0]->id);
+            }
+
+            $student_game->student_point = $request->student_point;
+            $student_game->game_id = $request->game_id;
+            $student_game->student_id = $student[0]->id;
+
+            $student_game->save();
+            $response->success = true;
+            $response->message = "Score saved";
+        } catch (Throwable $e) {
+            $response->success = false;
+            $response->message = $e;
+        }
+
+        return response()->json($response);
+    }
 }
