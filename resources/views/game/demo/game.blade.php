1aab42155b0a70440165842527f0bbb8f0f1e05b
mohdhafizp67 <mohdhafizp67@gmail.com>
2021-03-30 16:37:58 +0800

    push test

--- resources/views/game/demo/game.blade.php
+++ resources/views/game/demo/game.blade.php
@@ -374,7 +374,7 @@
 
               <div class="slider-single">
 
-                <a href="#" onclick="ajaxGame(1)"><img id="mobile" class="slider-single-image" src="{{ asset('concept/images/permainan/Arena.png') }}" alt="tutorial" /></a>
+                <a href="https://drive.google.com/u/1/uc?export=download&confirm=7EpR&id=1VK63AENjBDO9GWa3KFKRwk6U4JvPw2oi" onclick="ajaxGame(1)"><img id="mobile" class="slider-single-image" src="{{ asset('concept/images/permainan/Arena.png') }}" alt="tutorial" /></a>
                 <a href="http://161.35.227.188/drone_tutorial?secret={{base64_encode(auth()->user()->ic_number)}}" onclick="ajaxGame(1)"><img id="desktop" class="slider-single-image" src="{{ asset('concept/images/permainan/Arena.png') }}" alt="tutorial" /></a>
 
 
