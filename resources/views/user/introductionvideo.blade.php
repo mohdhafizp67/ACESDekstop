fb3bccedcca2dc28f0f642aa2f2bb1cf3a200008
mohdhafiz <61405917+mohdhafizp67@users.noreply.github.com>
2021-06-16 10:19:58 +0800

    push routes

--- resources/views/user/introductionvideo.blade.php
+++ resources/views/user/introductionvideo.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <html style="background-color: #032c41">
 <head>
+
   <link rel="stylesheet" href="{{ asset('css/Login.css') }} ">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
