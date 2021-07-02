c61b8386ebb58563315804701973f621305499d5
mohdhafizp67 <mohdhafizp67@gmail.com>
2021-07-02 10:27:08 +0800

    push baru

--- resources/views/layouts/app-student-qbadminui.blade.php
+++ resources/views/layouts/app-student-qbadminui.blade.php
@@ -403,9 +403,10 @@ element.requestFullscreen()
 	});
     var viewMode = getCookie("view-mode");
 if(viewMode == "desktop"){
-    viewport.setAttribute('content', 'width=1024');
+    viewport.setAttribute('content', 'width=980');
 }else if (viewMode == "mobile"){
-    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+    // viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+    viewport.setAttribute('content', 'width=980');
 }
     }
 
