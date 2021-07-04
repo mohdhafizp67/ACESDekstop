0047e163268e6224e32ff739f4cb0d6b874e07e2
luqmanulhakim96 <hakim9608@gmail.com>
2021-07-04 10:51:50 +0800

    admin fixes

--- resources/views/admin/others/user-list/list.blade.php
+++ resources/views/admin/others/user-list/list.blade.php
@@ -153,7 +153,7 @@
           }
         </script>
 
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
 
         <script type="text/javascript">
         $("document").ready(function(){
