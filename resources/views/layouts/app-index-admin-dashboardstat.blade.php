09e02040a2d5be1561aaa5c2adbb60931d38f303
luqmanulhakim96 <hakim9608@gmail.com>
2021-07-04 13:36:06 +0800

    edit log

--- resources/views/layouts/app-index-admin-dashboardstat.blade.php
+++ resources/views/layouts/app-index-admin-dashboardstat.blade.php
@@ -558,7 +558,7 @@
         var table = $('#table_data').DataTable({
           "responsive" : true,
           "dom": 'Bfrtip',
-          "order": [[ 4, "desc" ]],
+          "order": [[ 8, "desc" ]],
           "buttons": [
               'excel', 'pdf', 'print'
           ],
@@ -567,24 +567,6 @@
         });
         table.on( 'order.dt search.dt', function () {
         table.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
-            cell.innerHTML = i+1;
-        } );
-    } ).draw();
-    });
-
-    $(function () {
-        var tables = $('#table_data_audit').DataTable({
-          "responsive" : true,
-          "dom": 'Bfrtip',
-          "order": [[ 4, "desc" ]],
-          "buttons": [
-              'excel', 'pdf', 'print'
-          ],
-          "scrollX": true,
-
-        });
-        tables.on( 'order.dt search.dt', function () {
-        tables.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
             cell.innerHTML = i+1;
         } );
     } ).draw();
