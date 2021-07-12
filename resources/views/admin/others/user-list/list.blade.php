ee2cb0ec7adf62a924e124b555f83d843c11bc2f
luqmanulhakim96 <hakim9608@gmail.com>
2021-07-12 18:23:21 +0800

    Update list.blade.php

--- resources/views/admin/others/user-list/list.blade.php
+++ resources/views/admin/others/user-list/list.blade.php
@@ -66,17 +66,17 @@
                                             <td>{{ $data->lesson_progress ?? 0 }}</td>
                                             <td>{{ $data->quiz_progress ?? 0 }}</td>
                                             <td>{{ $data->game_progress ?? 0 }}</td>
-                                            @if ($data->total_progress)
+                                            {{-- @if ($data->total_progress)
                                                 <td>{{ number_format($data->total_progress, 2) }}
                                                 </td>
                                             @else
                                                 <td> 0 </td>
-                                            @endif
+                                            @endif --}}
                                         @else
                                             <td> 0 </td>
                                             <td> 0 </td>
                                             <td> 0 </td>
-                                            <td> 0.00 </td>
+                                            {{-- <td> 0.00 </td> --}}
                                         @endif
                                         @if ($data->is_admin == 1)
                                             <td><span class="badge badge-pill badge-brand">Admin</span></td>
